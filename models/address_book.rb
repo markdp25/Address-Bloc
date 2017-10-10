371f2a978901ab4e8fc0c51f16c2a32c85f28a23
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-10-10 17:31:46 -0400

    Checkpoint 5 coursework

--- models/address_book.rb
+++ models/address_book.rb
@@ -8,16 +8,16 @@ require_relative 'entry'
      end
 
    def add_entry(name, phone_number, email)
-     # #9
+
      index = 0
      entries.each do |entry|
-     # #10
+
        if name < entry.name
          break
        end
        index+= 1
      end
-     # #11
+
      entries.insert(index, Entry.new(name, phone_number, email))
    end
  end
