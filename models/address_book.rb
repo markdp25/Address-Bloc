33bfa5d2fcccef0f2addaf2d92e1ea0b764604a6
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-10-09 22:15:47 -0400

    Checkpoint 4 Assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -7,6 +7,17 @@ require_relative 'entry'
        @entries = []
      end
 
+     def remove_entry(name, phone_number, email)
+        deleted_entry = nil
+
+        @entries.each do |entry|
+            if name == entry.name && phone_number == entry.phone_number && email == entry.email
+                deleted_entry = entry
+            end
+        end
+        @entries.delete(deleted_entry)
+     end
+
    def add_entry(name, phone_number, email)
      # #9
      index = 0
